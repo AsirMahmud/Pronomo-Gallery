5f535849cc1e1a911afab9d655ad87534c7d6f85
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2024-09-05 15:04:31 +0600

    sdfsd

--- components/home/HeroSlider.vue
+++ components/home/HeroSlider.vue
@@ -2,7 +2,7 @@
     <swiper :effect="'coverflow'" :grabCursor="true" :centeredSlides="true" :autoplay="{
         delay: 1000,  // Time between slides in milliseconds
         disableOnInteraction: false  // Continue autoplay after user interactions
-    }" :slidesPerView="'auto'" :coverflowEffect="{
+    }" :slidesPerView="'4'" :coverflowEffect="{
         rotate: 50,
         stretch: 0,
         depth: 100,
@@ -33,7 +33,7 @@ const images = [
     'https://swiperjs.com/demos/images/nature-6.jpg',
     'https://swiperjs.com/demos/images/nature-7.jpg',
     'https://swiperjs.com/demos/images/nature-8.jpg',
-    'https://swiperjs.com/demos/images/nature-9.jpg'
+    'https://swiperjs.com/demos/images/nature-2.jpg',
 ];
 </script>
 
